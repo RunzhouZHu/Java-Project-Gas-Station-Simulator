5eb98879cd70cd24b038bc0a9648695ee025e53a
Runzhou Zhu <runzhouz@metropolia.fi>
2024-12-02 19:28:25 +0200

    update file structure

--- Simulator/src/main/java/view/simulatorView.java
+++ Simulator/src/main/java/view/simulatorView.java
@@ -3,12 +3,6 @@ package view;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
 
